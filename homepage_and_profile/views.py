149e8e87d42271cf052d4bb45c9c78982740ed2f
NikitaPash <pashchuknik@gmail.com>
2024-08-18 23:04:24 +0300

    Add layout and simple logic for user profile

--- homepage_and_profile/views.py
+++ homepage_and_profile/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+
+from user_auth.models import User
 
 
 def home(request):
     return render(request, 'homepage_and_profile/homepage.html')
+
+
+@login_required
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    context = {'user_profile': user}
+    return render(request, 'homepage_and_profile/profile.html', context)
