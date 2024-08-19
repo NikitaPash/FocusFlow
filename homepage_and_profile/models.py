c2aa2f044cad42c361febfc54c11812819d4b5ae
NikitaPash <pashchuknik@gmail.com>
2024-08-19 14:39:20 +0300

    Configure logic for storing and displaying user profile data

--- homepage_and_profile/models.py
+++ homepage_and_profile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from user_auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.TextField(max_length=150, blank=True)
+    bio = models.TextField(max_length=160, blank=True)
+    website = models.URLField(max_length=200, blank=True)
+    github = models.URLField(max_length=200, blank=True)
+    linkedin = models.URLField(max_length=200, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
--- homepage_and_profile/views.py
+++ homepage_and_profile/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from homepage_and_profile.forms import ProfileForm
 from user_auth.models import User
 
 
@@ -9,7 +10,23 @@ def home(request):
 
 
 @login_required
-def profile(request, username):
-    user = get_object_or_404(User, username=username)
-    context = {"user_profile": user}
+def profile(request, user_id, profile_form=None):
+    user = get_object_or_404(User, pk=user_id)
+    context = {"username": user.username, "user_profile": user.profile}
+
+    if profile_form:
+        context["profile_form"] = profile_form
+
     return render(request, "homepage_and_profile/profile.html", context)
+
+
+@login_required
+def update_profile(request, user_id):
+    form = ProfileForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("profile")
+
+    context = {"form": form}
+    return redirect("profile", profile_form=form)
